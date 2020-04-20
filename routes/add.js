123a8baa169bd1f9530eac6556f776d13d8e2cc9
Julia-Dubenchuk <dubenchuk6@gmail.com>
2020-04-20 21:55:56 +0300

    start test project node.js and express

---
+++ routes/add.js
@@ -0,0 +1,18 @@
+const { Router } = require("express");
+const Course = require("../models/course");
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.render("add", {
+    title: "Add a course",
+    isAdd: true,
+  });
+});
+
+router.post("/", async (req, res) => {
+  const course = new Course({ ...req.body });
+  await course.save();
+  res.redirect("/courses");
+});
+
+module.exports = router;
